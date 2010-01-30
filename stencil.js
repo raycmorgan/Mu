1a4ef8c9b167971f27f23936e78104cd9208cd6e
RayMorgan <ray@simple-apps.com>
2010-01-29 21:10:41 -0800

    Added stencil.js. What is stencil.js? You will find out soon...

---
+++ stencil.js
@@ -0,0 +1,20 @@
+var Mu = require('./lib/mu');
+
+Mu.templateRoot = '';
+Mu.templateExtension = '';
+
+exports.compile = function (filename, promise) {
+  Mu.compile(filename)
+    .addCallback(function (compiled) {
+      promise.emit('success', compiled);
+    })
+    .addErrback(function (e) {
+      promise.emit('error', e);
+    });
+}
+
+exports.render = function (ctx, compiled, stream, options) {
+  compiled(ctx, options)
+    .addListener('data', function (chunk) { stream.write(chunk); })
+    .addListener('eof', function () { stream.close(); });
+}
