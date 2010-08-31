5d413c781a21d37e3ebe1e999421e524a8d0a0ed
RayMorgan <ray@simple-apps.com>
2010-08-31 12:04:21 -0700

    Removed unused old files

--- compiler.js
+++
@@ -1,9 +0,0 @@
-var sys = require('sys');
-
-exports.compile = function (tokens) {
-  return compile(tokens);
-}
-
-function compile(tokens) {
-  var compiled = {strings: [], buffers: [], tokens: tokens, }
-}
